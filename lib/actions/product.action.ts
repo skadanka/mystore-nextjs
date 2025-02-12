97b05f1ad2e21de48182266725116f70cc503309
Eden Abuhatzeira <rylynn2@gmail.com>
2025-02-12 10:58:33 +0200

    prisma neon adapter added

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -1,11 +1,11 @@
 'use server';
-import { PrismaClient } from '@prisma/client';
+
+import { prisma } from '.@/db/prisma';
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_COUNT } from '../constants';
 
 // Get latest products
 export async function getLatestProduct() {
-    const prisma = new PrismaClient();
     const products = await prisma.product.findMany({
         take: LATEST_PRODUCTS_COUNT,
         orderBy: {
